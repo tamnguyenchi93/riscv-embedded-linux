f771106fc78a13b2f7843e2f03deef3da595d3b2
tamnguyenchi93 <tam04101993@gmail.com>
2021-07-16 17:22:54 +0000

    Update: environment script

--- riscv64-env.sh
+++ riscv64-env.sh
@@ -1,2 +1,3 @@
 export CROSS_COMPILE=riscv64-linux-
-export PATH=$HOME/toolchain/riscv64-buildroot-linux-musl_sdk-buildroot/bin:$PATH
+export PATH=${WORKING_DIR}/toolchain/riscv64-buildroot-linux-musl_sdk-buildroot/bin:$PATH
+export ARCH=riscv
